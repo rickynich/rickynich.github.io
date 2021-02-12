364e51aaa2d8d43b932d331289afa9228dc7b1b4
Nick Richard <nrich414@gmail.com>
2021-02-11 19:03:17 -0500

    avatar pic change, CMIYC thumbnail add

--- my-gatsby-project/src/components/Gallery/constants/defaultImages.js
+++ my-gatsby-project/src/components/Gallery/constants/defaultImages.js
@@ -4,7 +4,7 @@ import full03 from '../../../assets/images/fulls/03.jpg'
 import full04 from '../../../assets/images/fulls/04.jpg'
 import full05 from '../../../assets/images/fulls/05.jpg'
 import full06 from '../../../assets/images/fulls/06.jpg'
-import thumb02 from '../../../assets/images/thumbs/02.jpg'
+import routemap from '../../../assets/images/thumbs/routemap.jpg'
 import hanziu from '../../../assets/images/thumbs/hanziu.jpg'
 import thumb03 from '../../../assets/images/thumbs/03.jpg'
 import thumb04 from '../../../assets/images/thumbs/04.jpg'
@@ -23,36 +23,36 @@ export const DEFAULT_IMAGES = [
     {
       id: '2',
       source: full02,
-      thumbnail: thumb02,
-      caption: 'Photo 2',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      thumbnail: routemap,
+      caption: 'Catch Me If You Can',
+      description: 'A fun, competitive app that lets users create and keep track of exercise routes - wherever they are!',
     },
     {
       id: '3',
       source: full03,
       thumbnail: thumb03,
-      caption: 'Photo 3',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+      caption: 'Burn Reads',
+      description: 'A good reads clone, only the opposite.',
     },
-    {
-      id: '4',
-      source: full04,
-      thumbnail: thumb04,
-      caption: 'Photo 4',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-    },
-    {
-      id: '5',
-      source: full05,
-      thumbnail: thumb05,
-      caption: 'Photo 5',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-    },
-    {
-      id: '6',
-      source: full06,
-      thumbnail: thumb06,
-      caption: 'Photo 6',
-      description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-    }
+    // {
+    //   id: '4',
+    //   source: full04,
+    //   thumbnail: thumb04,
+    //   caption: 'Photo 4',
+    //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    // },
+    // {
+    //   id: '5',
+    //   source: full05,
+    //   thumbnail: thumb05,
+    //   caption: 'Photo 5',
+    //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    // },
+    // {
+    //   id: '6',
+    //   source: full06,
+    //   thumbnail: thumb06,
+    //   caption: 'Photo 6',
+    //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    // }
 ]
