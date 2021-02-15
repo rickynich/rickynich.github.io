2e032089f583ffcf29cda56d4e9575a5b7e40389
Nick Richard <nrich414@gmail.com>
2021-02-15 13:02:39 -0500

    back to default images..

--- my-gatsby-project/src/components/Gallery/constants/defaultImages.js
+++ my-gatsby-project/src/components/Gallery/constants/defaultImages.js
@@ -7,36 +7,39 @@ import full06 from '../../../assets/images/fulls/06.jpg'
 // import routemap from '../../../assets/images/thumbs/routemap.JPG'
 // import hanziu from '../../../assets/images/thumbs/hanziu.JPG'
 // import burnreads from '../../../assets/images/thumbs/burnreads.JPG'
-import burn from '../../../assets/images/thumbs/burn.png'
+// import burn from '../../../assets/images/thumbs/burn.png'
+import thumb01 from '../../../assets/images/thumbs/01.jpg'
+import thumb02 from '../../../assets/images/thumbs/02.jpg'
+import thumb03 from '../../../assets/images/thumbs/03.jpg'
 import thumb04 from '../../../assets/images/thumbs/04.jpg'
 import thumb05 from '../../../assets/images/thumbs/05.jpg'
 import thumb06 from '../../../assets/images/thumbs/06.jpg'
 
 
 export const DEFAULT_IMAGES = [
-  // {
-  //   id: '1',
-  //   source: full01,
-  //   thumbnail: hanziu,
-  //   caption: 'Hanzi U',
-  //   description: 'A strightforward Chinese character learning app.',
-  //   live_link: 'https://hanzi-u-app.herokuapp.com/landing',
-  //   github: 'https://github.com/rickynich/Hanzi-U',
-  // },
-  // {
-  //   id: '2',
-  //   source: full02,
-  //   thumbnail: routemap,
-  //   caption: 'Catch Me If You Can',
-  //   description:
-  //     'A fun, competitive app that lets users create and keep track of exercise routes - wherever they are!',
-  //   live_link: 'https://catch-me-if-you-cann.herokuapp.com/',
-  //   github: 'https://github.com/Kbart2401/Catch-me-if-you-can',
-  // },
+  {
+    id: '1',
+    source: full01,
+    thumbnail: thumb01,
+    caption: 'Hanzi U',
+    description: 'A strightforward Chinese character learning app.',
+    live_link: 'https://hanzi-u-app.herokuapp.com/landing',
+    github: 'https://github.com/rickynich/Hanzi-U',
+  },
+  {
+    id: '2',
+    source: full02,
+    thumbnail: thumb02,
+    caption: 'Catch Me If You Can',
+    description:
+      'A fun, competitive app that lets users create and keep track of exercise routes - wherever they are!',
+    live_link: 'https://catch-me-if-you-cann.herokuapp.com/',
+    github: 'https://github.com/Kbart2401/Catch-me-if-you-can',
+  },
   {
     id: '3',
     source: full03,
-    thumbnail: burn,
+    thumbnail: thumb03,
     caption: 'Burn Reads',
     description: 'A good reads clone, only the opposite.',
     live_link: '#',
